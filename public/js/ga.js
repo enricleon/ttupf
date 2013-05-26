224f0163de853a3a34749aa88691512c290f8185
Enric <nothingbuttumbleweed@gmail.com>
2013-05-26 09:06:24 +0200

    google analytics

---
+++ public/js/ga.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Enric
+ * Date: 26/05/13
+ * Time: 9:05
+ * To change this template use File | Settings | File Templates.
+ */
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-41220176-1', 'ttupf.com');
+ga('send', 'pageview');
+
+</script>
