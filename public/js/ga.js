46112af776b754a7fe6288bfff06a9afd8e162d3
Enric <nothingbuttumbleweed@gmail.com>
2013-05-27 09:49:11 +0200

    Google Analytics

--- public/js/ga.js
+++ public/js/ga.js
@@ -5,7 +5,7 @@
  * Time: 9:05
  * To change this template use File | Settings | File Templates.
  */
-<script>
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -13,5 +13,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-41220176-1', 'ttupf.com');
 ga('send', 'pageview');
-
-</script>
