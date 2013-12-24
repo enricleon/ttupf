a89ee1d60e83246eafb087a49fd7e03767e88bcf
Enric León <nothingbuttumbleweed@gmail.com>
2013-12-24 13:41:01 +0100

    Arreglat el formulari d'edicio d'enrollments.

--- public/js/enrollments.js
+++ public/js/enrollments.js
@@ -5,57 +5,61 @@
  * Time: 0:52
  * To change this template use File | Settings | File Templates.
  */
-$(document).ready(function() {
-    $(".ok_enrollment").on("click", function() {
-        $(this).attr("class", "icon-edit edit_enrollment");
 
-        var current_enrollment = $(this).parents("tr");
-        var current_enrollment_id = $(current_enrollment).find(".enrollment_id").attr("value");
-        var ce_seminar_group = $(current_enrollment).find(".editable[name=\"seminar_group\"]").attr("value");
-        var ce_practicum_group = $(current_enrollment).find(".editable[name=\"practicum_group\"]").attr("value");
+var ok_enrollment_click = function() {
+    $(this).hide();
 
-        $(".edit_enrollment").each(function(){
-            $(this).show();
-        });
+    var current_enrollment = $(this).parents("tr");
+    var current_enrollment_id = $(current_enrollment).find(".enrollment_id").attr("value");
+    var ce_seminar_group = $(current_enrollment).find(".editable[name=\"seminar_group\"]").attr("value");
+    var ce_practicum_group = $(current_enrollment).find(".editable[name=\"practicum_group\"]").attr("value");
 
-        $(current_enrollment).find(".editable").each(function(){
-            $(this).attr("disabled", "disabled");
-        });
-
-        $(current_enrollment).css("background-color", "rgb(255, 255, 255)");
-
-        $.ajax({
-            url: "/api/enrollments",
-            type: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify({ enrollment: current_enrollment_id, seminar_group: ce_seminar_group, practicum_group: ce_practicum_group}),
-            dataType: 'json',
-            success: function(response) {
-                console.log("success: " + JSON.stringify(response));
-            },
-            failure: function(response) {
-                console.log("failure");
-            },
-            error: function() {
-                console.log("error");
-            },
-            complete: function() {
-                console.log("complete");
-            }
-        });
-    });
-
-    $(".edit_enrollment").on("click", function() {
-        $(".edit_enrollment").each(function(){
-            $(this).hide();
-        });
-        var current_enrollment = $(this).parents("tr");
-        var current_enrollment_id = $(current_enrollment).find(".enrollment_id").attr("value");
-        $(current_enrollment).find(".editable").each(function(){
-            $(this).removeAttr("disabled");
-        });
-        $(this).attr("class", "icon-ok ok_enrollment");
+    $(".edit_enrollment").each(function(){
         $(this).show();
-        $(this).parents("tr").css("background-color", "rgb(245, 245, 245)");
     });
+
+    $(current_enrollment).find(".editable").each(function(){
+        $(this).attr("disabled", "disabled");
+    });
+
+    $(current_enrollment).css("background-color", "rgb(255, 255, 255)");
+
+    $.ajax({
+        url: "/api/enrollments",
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify({ enrollment: current_enrollment_id, seminar_group: ce_seminar_group, practicum_group: ce_practicum_group}),
+        dataType: 'json',
+        success: function(response) {
+            console.log("success: " + JSON.stringify(response));
+        },
+        failure: function(response) {
+            console.log("failure");
+        },
+        error: function() {
+            console.log("error");
+        },
+        complete: function() {
+            console.log("complete");
+        }
+    });
+};
+
+var edit_enrollment_click = function() {
+    $(".edit_enrollment").each(function(){
+        $(this).hide();
+    });
+    var current_enrollment = $(this).parents("tr");
+    $(current_enrollment).find(".editable").each(function(){
+        $(this).removeAttr("disabled");
+    });
+
+    var ok_enrollment = $(current_enrollment).find(".ok_enrollment");
+    ok_enrollment.show();
+    $(this).parents("tr").css("background-color", "rgb(245, 245, 245)");
+};
+
+$(document).ready(function() {
+    $(".edit_enrollment").on("click", edit_enrollment_click);
+    $(".ok_enrollment").on("click", ok_enrollment_click);
 });
